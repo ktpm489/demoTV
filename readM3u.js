ac5827dcf02437c459cdb4f2789fb6b6c6b09680
ktpm489 <hoangvu@bacoor.co>
2019-08-26 16:32:33 +0700

    Add checklink tool

--- readM3u.js
+++ readM3u.js
@@ -1,9 +1,26 @@
 const M3U8FileParser = require('m3u8-file-parser');
 const fs = require('fs');
-const content = fs.readFileSync('./example.m3u', { encoding: 'utf-8' });
 
-const reader = new M3U8FileParser();
-reader.read(content);
-console.log('GetResult', reader.getResult().segments)
- // Get the parse result
-reader.reset(); // Optional, If you want to parse a new file, call reset(
+function readData () {
+    const content = fs.readFileSync('./example.m3u', { encoding: 'utf-8' });
+    const reader = new M3U8FileParser();
+    reader.read(content);
+    // console.log('GetResult', reader.getResult().segments)
+    // Get the parse result
+    let arrNew = []
+    reader.getResult().segments.map((item, index) => {
+        // console.log('item', item.inf['tvg-logo'])
+        arrNew.push({
+            title: item.inf.title,
+            link: item.url,
+            logo: item.inf['tvg-logo']
+        })
+    }
+    )
+    reader.reset(); // Optional, If you want to parse a new file, call reset
+    console.log('arraData', JSON.stringify(arrNew))
+    fs.writeFileSync('./formatFile.json', JSON.stringify(arrNew), { encoding: 'utf-8' })
+    return arrNew
+}
+
+readData()
--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 
 import ReactHLS from 'react-hls';
-let arr = ['http://hjyjrvmlsk.vcdn.com.vn/hls/elgfjdh/index.m3u8',
-  'https://hjyjrvmlsk.vcdn.com.vn/hls/m0hlc1s/index.m3u8',
-  'https://hjyjrvmlsk.vcdn.com.vn/hls/idmwifi/index.m3u8',
-  'https://hjyjrvmlsk.vcdn.com.vn/hls/hktebdo/index.m3u8',
-  'http://185.246.209.109:8080/GOL_TV/index.m3u8'
-] 
+import * as arrData from './formatFile.json'
+// let arr = ['http://hjyjrvmlsk.vcdn.com.vn/hls/elgfjdh/index.m3u8',
+//   'https://hjyjrvmlsk.vcdn.com.vn/hls/m0hlc1s/index.m3u8',
+//   'https://hjyjrvmlsk.vcdn.com.vn/hls/idmwifi/index.m3u8',
+//   'https://hjyjrvmlsk.vcdn.com.vn/hls/hktebdo/index.m3u8',
+//   'http://185.246.209.109:8080/GOL_TV/index.m3u8'
+// ] 
+
+let arr = arrData.default
 class HLSPage extends Component {
   
   constructor(props){
     super(props)
     this.state = {
-      currentLink : arr[0],
+      currentLink : arr[0].link,
       currentIndex : 0,
+      currentData : arr[0],
       errList : [],
       okList : []
     }
 }
 
   onError = (e, data) => {
-    console.log('e', e, 'data', data)
+    // console.log('e', e, 'data', data)
     if (data) {
-      let { currentIndex, errList, currentLink } = this.state
+      let { currentIndex, errList, currentData } = this.state
       if (currentIndex < arr.length) {
-        errList.push(currentLink)
+        errList.push(currentData)
         let newIndex = currentIndex + 1
         this.setState({
           currentIndex: newIndex,
-          currentLink: arr[newIndex],
+          currentLink: arr[newIndex].link,
+          currentData: arr[newIndex],
           errList: errList
         })
       }
@@ -37,14 +42,15 @@ class HLSPage extends Component {
   }
 
   onSuccess = (url) => {
-    console.log('url', url)
-    let { currentIndex, okList, currentLink } = this.state
+    // console.log('url', url)
+    let { currentIndex, okList, currentData } = this.state
     if (currentIndex < arr.length) {
-      okList.push(currentLink)
+      okList.push(currentData)
       let newIndex = currentIndex + 1
       this.setState({
         currentIndex: newIndex,
-        currentLink: arr[newIndex],
+        currentData: arr[newIndex],
+        currentLink: arr[newIndex].link,
         okList: okList
       })
     }
@@ -52,6 +58,7 @@ class HLSPage extends Component {
 
   render() {
     const {currentLink} = this.state
+    // console.log('current arr',arr)
     console.log(this.state)
     return (
       // <Player>
@@ -62,7 +69,7 @@ class HLSPage extends Component {
       //   {/* <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" /> */}
       // </Player>
       <div>
-        <ReactHLS url={currentLink} constrols autoplay onError={this.onError} onSuccess={this.onSuccess}/>
+        <ReactHLS url={'http://113.161.6.157:8081/hls-live/livepkgr/_definst_/liveevent/livestream.m3u8'} constrols autoplay onError={this.onError} onSuccess={this.onSuccess}/>
       </div>
 
     );
